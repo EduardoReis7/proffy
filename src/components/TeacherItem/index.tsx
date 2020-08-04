c87bda31b8faa71264ff2236a27a14571e107500
EduardoReis7 <eduardodovigireis@gmail.com>
2020-08-04 01:24:52 -0300

    Commit inicial

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'; 
+
+import './styles.css';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQETiUqKaFGlLg/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=KvszV-CKLmTa6VmAUAW8UEFPN01I1ywlCq52yeU_DO8" alt="Eduardo Reis"/>
+                <div>
+                    <strong>Eduardo Reis</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br/><br/>
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 2000 pessoas já passaram por uma das minhas explosões.
+            </p>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp"/>
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
